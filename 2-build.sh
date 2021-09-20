b43aa72e161d82a5c9e8823041ff11446e1232b6
gary.archer <gary.archer@WORKMAC.local>
2021-09-20 15:10:05 +0200

    Updates

--- 2-build.sh
+++ 2-build.sh
@@ -27,10 +27,19 @@ fi
 #
 # Build the API Docker Container
 #
-cd ..
-docker build -f ./api/Dockerfile -t mutual-tls-api:1.0.0 .
+docker build -f ./Dockerfile -t mutual-tls-api:1.0.0 .
 if [ $? -ne 0 ]; then
   echo 'Problem encountered building the API docker container'
+  exit
+fi
+
+#
+# Build the customized NGINX Docker Container and its plugin
+#
+cd ../docker/reverse-proxy
+docker build -f ./Dockerfile -t custom_openresty:1.19.3.1-8-bionic .
+if [ $? -ne 0 ]; then
+  echo 'Problem encountered building the Custom NGINX docker container'
   exit
 fi
 
--- 4-run-test-client.sh
+++ 4-run-test-client.sh
@@ -8,11 +8,9 @@ IDENTITY_SERVER_BASE_URL=https://login.example.com/oauth/v2
 API_BASE_URL=https://api.example.com:444/api
 CLIENT_ID=partner-client
 CLIENT_SECRET=Password1
-INTROSPECT_CLIENT_ID=introspect-client
-INTROSPECT_CLIENT_SECRET=Password1
 RESPONSE_FILE=./.test/response.txt
 
-mkdir -p ./.test
+mkdir -p .test
 
 #
 # Act as the client to get an opaque access token using Mutual TLS
@@ -33,35 +31,18 @@ if [ "$HTTP_STATUS" != '200' ]; then
 fi
 JSON=$(tail -n 1 $RESPONSE_FILE) 
 OPAQUE_ACCESS_TOKEN=$(jq -r .access_token <<< "$JSON")
-echo "Client successfully authenticated and received access token: $OPAQUE_ACCESS_TOKEN"
+echo "Client successfully authenticated and received an opaque access token"
 
 #
-# Act as the API gateway to introspect the token
 #
-echo "API gateway is introspecting the opaque access token ..."
-HTTP_STATUS=$(curl -s -X POST "$IDENTITY_SERVER_BASE_URL/oauth-introspect" \
-    -H "Accept: application/jwt" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "client_id=$INTROSPECT_CLIENT_ID" \
-    -d "client_secret=$INTROSPECT_CLIENT_SECRET" \
-    -d "token=$OPAQUE_ACCESS_TOKEN" \
-    -o $RESPONSE_FILE -w '%{http_code}')
-if [ "$HTTP_STATUS" != '200' ]; then
-  echo "*** API gateway experienced an introspection problem: $HTTP_STATUS"
-  exit
-fi
-JWT_ACCESS_TOKEN=$(tail -n 1 $RESPONSE_FILE) 
-echo "API gateway successfully retrieved a JWT access token"
-
+# Act as the client sending the opaque access token to the API
 #
-# Act as the client sending the JWT to the API
-#
-echo "Client is calling the example API over Mutual TLS ..."
+echo "Client is creating a transaction at the API using Mutual TLS and the opaque access token ..."
 HTTP_STATUS=$(curl -s -X POST "$API_BASE_URL/transactions" \
     --cert ./certs/example.client.pem \
     --key ./certs/example.client.key \
     --cacert ./certs/root.pem \
-    -H "Authorization: Bearer $JWT_ACCESS_TOKEN" \
+    -H "Authorization: Bearer $OPAQUE_ACCESS_TOKEN" \
     -H "Content-Type: application/json" \
     -o $RESPONSE_FILE -w '%{http_code}')
 if [ "$HTTP_STATUS" != '200' ]; then
@@ -69,5 +50,4 @@ if [ "$HTTP_STATUS" != '200' ]; then
   exit
 fi
 JSON=$(tail -n 1 $RESPONSE_FILE) 
-echo $JSON | jq
-echo "Client successfully called the API"
+echo "Client successfully created the API transaction using Mutual TLS"
