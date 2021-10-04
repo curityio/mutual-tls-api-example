96d24684d6c5d3a2e6d89ad8b40955c34ddcc3ba
gary.archer <gary.archer@WORKMAC.local>
2021-10-04 20:38:22 +0200

    Completed token binding plugin

--- docker/reverse-proxy/token-binding-plugin/token-binding-plugin.lua
+++ docker/reverse-proxy/token-binding-plugin/token-binding-plugin.lua
@@ -1,11 +1,14 @@
 --
--- A LUA module to verify that the JWT sent over a Mutual TLS matches that provided at authentication
+-- A LUA module to verify token binding for a JWT sent via a Mutual TLS connection
 --
 
 local _M = {}
 local jwt = require 'resty.jwt'
 local pl_stringx = require "pl.stringx"
-local resty_string = require 'resty.string'
+local str = require 'resty.string'
+local sha256 = require 'resty.sha256'
+local b64 = require 'ngx.base64'
+local ssl = require 'ngx.ssl'
 
 --
 -- Return errors due to invalid requests or server technical problems
@@ -22,12 +25,12 @@ end
 --
 -- Return an error message to indicate an unauthorized request
 --
-local function unauthorized_error_response(config)
-    error_response(ngx.HTTP_UNAUTHORIZED, "unauthorized", "Missing, invalid or expired access token", config)
+local function unauthorized_error_response()
+    error_response(ngx.HTTP_UNAUTHORIZED, 'unauthorized', 'Missing, invalid or expired access token')
 end
 
 --
--- Parse the JWT and retrieve the cnf/x5t#S256 claim
+-- Parse the JWT and retrieve the cnf/x5t#S256 claim issued at the time of authentication
 --
 local function read_token_thumbprint(jwt_text)
     
@@ -40,45 +43,58 @@ local function read_token_thumbprint(jwt_text)
 end
 
 --
+-- Calculate the SHA256 hash of the client certificate received via Mutual TLS
+-- https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.8
+--
+local function get_sha256_thumbprint(certificate)
+
+    local der = ssl.cert_pem_to_der(certificate)
+    local hash = sha256:new()
+    hash:update(der)
+    local digest = hash:final()
+    return b64.encode_base64url(digest)
+end
+
+--
 -- The public entry point to verify sender constrained access token details
 --
 function _M.execute(config)
 
     if config.type ~= 'certificate-bound' then
-        ngx.log(ngx.WARN, "An invalid or unsupported type parameter was received")
-        error_response(ngx.HTTP_INTERNAL_SERVER_ERROR, "server_error", "Problem encountered processing the request")
+        ngx.log(ngx.WARN, 'An invalid or unsupported type parameter was received')
+        error_response(ngx.HTTP_INTERNAL_SERVER_ERROR, 'server_error', 'Problem encountered processing the request')
     end
 
     -- By this time the access token should be available as a JWT
-    local access_token = ngx.req.get_headers()["Authorization"]
+    local access_token = ngx.req.get_headers()['Authorization']
     if access_token then
-        access_token = pl_stringx.replace(access_token, "Bearer ", "", 1)
+        access_token = pl_stringx.replace(access_token, 'Bearer ', '', 1)
     end
 
     if not access_token then
-        ngx.log(ngx.WARN, "No access token was found in the Authorization bearer header")
+        ngx.log(ngx.WARN, 'No access token was found in the Authorization bearer header')
         unauthorized_error_response()
     end
 
     -- We should also have a client certificate
     if ngx.var.ssl_client_escaped_cert == nil then
-        ngx.log(ngx.WARN, "The request did not contain a valid client certificate")
+        ngx.log(ngx.WARN, 'The request did not contain a valid client certificate')
         unauthorized_error_response()
     end
 
     -- Read the thumbprint
-    local thumbprint = read_token_thumbprint(access_token)
-    if thumbprint == nil then
-        ngx.log(ngx.WARN, "Unable to parse the x5t#S256 from the received JWT access token")
+    local jwtThumbprint = read_token_thumbprint(access_token)
+    if jwtThumbprint == nil then
+        ngx.log(ngx.WARN, 'Unable to parse the x5t#S256 from the received JWT access token')
         unauthorized_error_response()
     end
-    
-    -- This gets a value of the following form and we need to create a SHA256 hash of it, so needs some manipulation
-    -- -----BEGIN%20CERTIFICATE-----%0AMIIDJjC
-    
-    ngx.log(ngx.WARN, "CERTIFICATE: " .. ngx.var.ssl_client_escaped_cert)
-    ngx.log(ngx.WARN, "CERTIFICATE HASH: " .. resty_string.sha256(ngx.var.ssl_client_escaped_cert))
-    ngx.log(ngx.WARN, "THUMBPRINT: "  .. thumbprint)
+
+    -- Calculate the SHA256 hash of the certificate / ssl_client_escaped_cert
+    local certThumbprint = get_sha256_thumbprint(ngx.var.ssl_client_raw_cert)
+    if certThumbprint ~= jwtThumbprint then
+        ngx.log(ngx.WARN, 'The client certificate details of the request and the JWT do not match')
+        unauthorized_error_response()
+    end
 end
 
 return _M
